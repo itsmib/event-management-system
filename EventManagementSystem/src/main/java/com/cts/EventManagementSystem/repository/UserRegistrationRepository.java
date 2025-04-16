f26995fe5e7b66c4f7ab32840d714030db1098c9
itsmib <noorulmisbah15@gmail.com>
2025-04-16 16:49:29 +0530

    Changing repository name

--- EventManagementSystem/src/main/java/com/cts/EventManagementSystem/repository/UserRegistrationRepository.java
+++ EventManagementSystem/src/main/java/com/cts/EventManagementSystem/repository/UserRegistrationRepository.java
@@ -1,4 +1,4 @@
-package com.cts.EventManagementSystem.repo;
+package com.cts.EventManagementSystem.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
